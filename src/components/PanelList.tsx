95f2bd47d6ef623d49c3ff1b0f24349c36f6bc3c
Machi <118187797+kamachi-dev@users.noreply.github.com>
2025-07-07 08:55:49 +0800

    1.1.0
    -add link icon

--- src/components/PanelList.tsx
+++ src/components/PanelList.tsx
@@ -1,4 +1,6 @@
 import { useInView } from 'react-intersection-observer'
+import '../styles/components/PanelList.css';
+
 
 interface Props {
     items: [React.ReactElement, string][]
@@ -10,6 +12,7 @@ function PanelList({ items }: Props) {
         <ul className='rainbowItems' ref={ref}>
             {items.map(([html, link]) =>
                 <li className={'rainbowItems-border ' + (inView ? 'show' : 'hide')}>
+                    <img src='/assets/images/link.png' alt='link' className={'panel-link ' + ((link == '') ? 'hidden' : 'show')} />
                     <div className='rainbowItems-content'><a href={link}>{html}</a></div>
                 </li>
             )}
--- src/pages/Education.tsx
+++ src/pages/Education.tsx
@@ -8,32 +8,40 @@ function Education() {
                 <li>Hackaton</li>
                 <li>A date with python 2023</li>
             </ul>
-        </>, 'Contests']
+        </>, '']
     ]
     const google: [React.ReactElement, string][] = [
         [<>
             <h5>Google Cloud Skills Boost Certificates</h5>
+            <p>Jul 13, 2024</p>
         </>, 'https://www.cloudskillsboost.google/public_profiles/8c7a8bb5-6bbf-4746-a105-1f4a540dfb3a/badges/9831621'],
         [<>
             <h5>Prepare Data for ML APIs on Google Cloud</h5>
+            <p>Jul 13, 2024</p>
         </>, 'https://www.cloudskillsboost.google/public_profiles/8c7a8bb5-6bbf-4746-a105-1f4a540dfb3a/badges/9831141'],
         [<>
             <h5>Build a Secure Google Cloud Network</h5>
+            <p>Jul 13, 2024</p>
         </>, 'https://www.cloudskillsboost.google/public_profiles/8c7a8bb5-6bbf-4746-a105-1f4a540dfb3a/badges/9830309'],
         [<>
             <h5>Set Up an App Dev Environment on Google Cloud</h5>
+            <p>Jul 13, 2024</p>
         </>, 'https://www.cloudskillsboost.google/public_profiles/8c7a8bb5-6bbf-4746-a105-1f4a540dfb3a/badges/9829948'],
         [<>
             <h5>Google Cloud Computing Foundations: Data, ML, and AI in Google Cloud</h5>
+            <p>Jul 11, 2024</p>
         </>, 'https://www.cloudskillsboost.google/public_profiles/8c7a8bb5-6bbf-4746-a105-1f4a540dfb3a/badges/9808730'],
         [<>
             <h5>Google Cloud Computing Foundations: Networking & Security in Google Cloud</h5>
+            <p>Jul 11, 2024</p>
         </>, 'https://www.cloudskillsboost.google/public_profiles/8c7a8bb5-6bbf-4746-a105-1f4a540dfb3a/badges/9806367'],
         [<>
             <h5>Google Cloud Computing Foundations: Infrastructure in Google Cloud</h5>
+            <p>Jul 10, 2024</p>
         </>, 'https://www.cloudskillsboost.google/public_profiles/8c7a8bb5-6bbf-4746-a105-1f4a540dfb3a/badges/9391528'],
         [<>
             <h5>Google Cloud Computing Foundations: Cloud Computing Fundamentals</h5>
+            <p>Jul 10, 2024</p>
         </>, 'https://www.cloudskillsboost.google/public_profiles/8c7a8bb5-6bbf-4746-a105-1f4a540dfb3a/badges/9389339']
     ]
     return (
