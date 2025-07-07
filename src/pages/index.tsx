9bc9d8a40826206ad4f802b665641238de7add08
kamachi-dev <angelo.kamachi03@gmail.com>
2025-07-07 18:20:02 +0800

    contact fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
 export { default as Home } from './Home'
 export { default as Education } from './Education'
 export { default as Skills } from './Skills'
+export { default as Contacts } from './Contacts'
