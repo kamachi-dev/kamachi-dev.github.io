7ccd4c794d72f8a66c9ebccfb590a23b47cf3677
Machi <Angelo.kamachi03@gmail.com>
2025-07-27 16:18:22 +0800

    1.4.1

--- src/services/client.ts
+++ src/services/client.ts
@@ -1,8 +1,6 @@
 import { createClient, SupabaseClient } from '@supabase/supabase-js';
 
 export const supabase: SupabaseClient = createClient(
-    //import.meta.env.VITE_SUPABASE_URL!,
-    //import.meta.env.VITE_SUPABASE_ANON_KEY!
-    'https://psgbsjzcgadniqruofak.supabase.co',
+    'https://www.machi.codes',
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBzZ2JzanpjZ2FkbmlxcnVvZmFrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTIxMjY3MjMsImV4cCI6MjA2NzcwMjcyM30.3dQNRkUIwXLaDy-J98dcRjOKWEYipVixAvMTw2PZDT8'
 )
